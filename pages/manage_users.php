f695f5858bc2ef548a989248220af22b4cf2c8a4
DizzyCheems <stine6595@gmail.com>
2025-10-16 22:41:07 +0800

    send pdate

--- pages/manage_users.php
+++ pages/manage_users.php
@@ -200,7 +200,7 @@ try {
     <?php include '../layout/navbar.php'; ?>
     <div class="container-fluid">
         <div class="row">
-            <?php include '../layout/officer_menubar.php'; ?>
+            <?php include '../layout/menubar.php'; ?>
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-4 border-bottom">
                     <h1 class="h2 text-primary">Manage Users</h1>
